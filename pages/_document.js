598cc3ae894bc889ea905549b4029d01dfa4baf9
jhonny4jnoHuchani <hynoku2004r@gmail.com.>
2025-12-03 14:56:01 -0400

    actualizado

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,41 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+// CORRECTO:
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Returns an object like: { html, head, errorHtml, chunks, styles }     
-    return renderPage();
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <html>
+      <Html lang="es">
+        {" "}
+        {/* AQUÍ: Usar <Html> en lugar de <html> */}
         <Head>
-          <title>Carrera de Arquitectura - UPEA</title>
+          {/* NO poner <title> aquí - se pone en cada página */}
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&family=Raleway:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          {/* Otros meta tags, favicon, etc. */}
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
-    )
+      </Html>
+    );
   }
 }
